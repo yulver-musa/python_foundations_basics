a82465cd700305da718fc14555730820abdd7f57
Yulver <yulver.musa@gmail.com>
2025-08-26 21:19:33 +0200

    Added a new file.

---
+++ zoo_shop.py
@@ -0,0 +1,11 @@
+dog_food = int(input())
+cat_food = int(input())
+
+dog_food_price = 2.5
+cat_food_price = 4
+
+dog_total = dog_food * dog_food_price
+cat_total = cat_food * cat_food_price
+
+total = dog_total + cat_total
+print(f'{total} lv.')
