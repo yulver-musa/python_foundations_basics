86e7ef5757bf1ddbfa8f1ba9182b57ba8e92bd82
Yulver <yulver.musa@gmail.com>
2025-08-26 21:12:05 +0200

    Added a new file.

---
+++ architect_hours.py
@@ -0,0 +1,7 @@
+name = input()
+projects = int(input())
+hours_per_project = 3
+
+total = projects * hours_per_project
+
+print(f'The architect {name} will need {total} hours to complete {projects} project/s.')
