e0c335fa275d6080a49e07c0caad5063166aa8b4
Yulver <yulver.musa@gmail.com>
2025-08-28 21:10:25 +0200

    Added a new comments.

--- vacation_booklist_2.py
+++ vacation_booklist_2.py
@@ -1,3 +1,7 @@
+# George must finish a book in time.
+# Input: pages, pages/hour, days.
+# Output: hours/day needed for reading.
+
 count_of_pages = int(input())
 pages_per_hour = int(input())
 count_of_days = int(input())
