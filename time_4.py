ebf0bce8b63d61c6173045c2b5cefc433f6697c4
Yulver <yulver.musa@gmail.com>
2025-09-03 21:08:51 +0200

    Added a new file.

---
+++ time_4.py
@@ -0,0 +1,19 @@
+hours = int(input())
+minutes = int(input())
+
+extra_minutes = 15
+
+hours_in_minutes = hours * 60
+
+total_minutes = hours_in_minutes + minutes + extra_minutes
+
+final_hours = total_minutes // 60
+if final_hours >= 24:
+    final_hours = final_hours - 24
+final_minutes = total_minutes % 60
+
+if final_minutes < 10:
+    print(f'{final_hours}:0{final_minutes}')
+else:
+    print(f'{final_hours}:{final_minutes}')
+
