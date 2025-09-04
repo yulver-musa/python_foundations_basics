918c1ad9610e46ea4443a6718a6d212455d89de9
Yulver <yulver.musa@gmail.com>
2025-09-04 20:36:01 +0200

    Added a new file.

---
+++ movie_4.py
@@ -0,0 +1,20 @@
+movie_budget = float(input())
+crew_member = int(input())
+costume_price = float(input())
+
+decor = movie_budget * 0.1
+if crew_member > 150:
+    costume_price = costume_price * 0.9
+
+total_costumes = crew_member * costume_price
+
+total_expenditures = total_costumes + decor
+
+difference = abs(movie_budget - total_expenditures)
+
+if movie_budget >= total_expenditures:
+    print("Action!")
+    print(f"Wingard starts filming with {difference:.2f} leva left.")
+else:
+    print("Not enough money!")
+    print(f"Wingard needs {difference:.2f} leva more.")
