2f1258efd5b3ddb0436b1bc0f7f68aa7258de246
Yulver <yulver.musa@gmail.com>
2025-09-09 21:29:09 +0200

    Added a new lines.

--- shopping_4.py
+++ shopping_4.py
@@ -12,3 +12,10 @@ cpu_cost = cpu_count * cpu_price
 ram_price = gpu_cost * 0.1
 ram_cost = ram_count * ram_price
 
+sub_cost = gpu_cost + cpu_cost + ram_cost
+
+if gpu_count > cpu_count:
+    sub_cost *= 0.85
+
+diff = abs(budget - sub_cost)
+
