4284ad1d21a74ac6e5d5f6be31e82d627afdc3bc
Yulver <yulver.musa@gmail.com>
2025-09-09 21:26:20 +0200

    Added a new file.

---
+++ shopping_4.py
@@ -0,0 +1,14 @@
+budget = float(input())
+gpu_count = int(input())
+cpu_count = int(input())
+ram_count = int(input())
+
+gpu_price = 250
+gpu_cost = gpu_count * gpu_price
+
+cpu_price = gpu_cost * 0.35
+cpu_cost = cpu_count * cpu_price
+
+ram_price = gpu_cost * 0.1
+ram_cost = ram_count * ram_price
+
