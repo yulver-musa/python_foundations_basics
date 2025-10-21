68c9743b78c6f9cd18155eda229479b1ef21b101
Yulver <yulver.musa@gmail.com>
2025-10-21 20:45:07 +0200

    Updated file

---
+++ working_day_or_not.py
@@ -0,0 +1,7 @@
+week_day = input()
+
+if week_day == "Monday" or "Tuesday" or "Wednesday" or "Thursday" or "Friday":
+    print("Working day")
+elif week_day == "Saturday" or "Sunday":
+    print("Weekend")
+else:
