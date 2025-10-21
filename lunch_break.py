7ba6c26e1903181af695e6ff5f06dd9dc1be621d
Yulver <yulver.musa@gmail.com>
2025-10-21 20:27:14 +0200

    Updated file

--- lunch_break.py
+++ lunch_break.py
@@ -6,9 +6,9 @@ lunch_duration = break_duration * 0.125
 rest_duration = break_duration * 0.25
 free_time = break_duration - lunch_duration - rest_duration
 
-difference = abs(break_duration - free_time)
+difference = abs(show_duration - free_time)
 
-if break_duration >= free_time:
+if free_time >= show_duration:
     print(f"You have enough time to watch {tv_show} and left with {difference} minutes free time.")
 else:
     print(f"You don't have enough time to watch {tv_show}, you need {difference} more minutes.")
