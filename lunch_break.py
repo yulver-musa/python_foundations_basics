d54ef37085b54aae777f22e2e83848dd09665d35
Yulver <yulver.musa@gmail.com>
2025-10-21 20:32:04 +0200

    Updated file

--- lunch_break.py
+++ lunch_break.py
@@ -1,3 +1,5 @@
+import math
+
 tv_show = input()
 show_duration = int(input())
 break_duration = int(input())
@@ -9,6 +11,6 @@ free_time = break_duration - lunch_duration - rest_duration
 difference = abs(show_duration - free_time)
 
 if free_time >= show_duration:
-    print(f"You have enough time to watch {tv_show} and left with {difference} minutes free time.")
+    print(f"You have enough time to watch {tv_show} and left with {math.ceil(difference)} minutes free time.")
 else:
-    print(f"You don't have enough time to watch {tv_show}, you need {difference} more minutes.")
+    print(f"You don't have enough time to watch {tv_show}, you need {math.ceil(difference)} more minutes.")
