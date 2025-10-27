fe021f9e4519175bdf014605df21d9ebc1efbac0
Yulver <yulver.musa@gmail.com>
2025-10-27 22:18:36 +0200

    Updating file.

--- working_hours.py
+++ working_hours.py
@@ -1,8 +1,8 @@
 hour = int(input())
 day = input()
 
-if 10 <= hour <= 18:
-    if day == "Monday" or day == "Tuesday" or day == "Wednesday":
-        print("open")
-    elif day == "Sunday":
-        print("closed")
+if 10 <= hour <= 18 and day == "Monday" or day == "Tuesday" or day == "Wednesday" \
+        or day == "Thursday" or day == "Friday" or day == "Saturday":
+    print("open")
+else:
+    print("closed")
