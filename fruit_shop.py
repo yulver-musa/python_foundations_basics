6fd23ee8069b213718cc4f8e5f531b89611ff658
Yulver <yulver.musa@gmail.com>
2025-11-13 20:58:06 +0200

    Added a new file.

---
+++ fruit_shop.py
@@ -0,0 +1,24 @@
+fruit = input()
+day = input()
+qty = int(input())
+
+price = 0
+
+if day == "Monday" or day == "Tuesday" or day == "Wednesday" or day == "Thursday" or day == "Friday":
+    if fruit == "banana":
+        price = 2.50
+    elif fruit == "apple":
+        price = 1.20
+    elif fruit == "orange":
+        price = 0.85
+    elif fruit == "grapefruit":
+        price = 1.45
+    elif fruit == "kiwi":
+        price = 2.70
+    elif fruit == "pineapple":
+        price = 5.50
+    elif fruit == "grapes":
+        price = 3.85
+    else:
+        print("error")
+
