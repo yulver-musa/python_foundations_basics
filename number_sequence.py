a2e0d6ffce8d3ab3125fded6666c7343b92d6c0e
Yulver <yulver.musa@gmail.com>
2025-11-15 12:03:50 +0200

    Added a new file.

---
+++ number_sequence.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+min_number = float('inf')
+max_number = float('-inf')
+
+for i in range(n):
+    number = int(input())
+
+    if number < min_number:
+        min_number = number
+
+    if number > max_number:
+        max_number = number
+
+print(f"Max number: {max_number}")
+print(f"Min number: {min_number}")
